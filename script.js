66d1cd2db2cce4ed6862673b178d7dc1041eb3a7
BulyginK <buligin.k@gmail.com>
2022-03-25 15:31:13 +0300

    Выполнил усложненное задание 2 урока

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Для выполненения усложненных заданий
+let num = 266219;
+let str = String(num);
+let arr = str.split('')
+let multiply = 1;
+let degree;
+let strDegree;
+
+for (let i = 0; i < arr.length; i++) {
+    multiply *= Number(arr[i])
+}
+
+console.log(multiply);
+
+degree = multiply ** 3
+strDegree = String(degree);
+
+console.log(strDegree.substring(0, 2)); 
