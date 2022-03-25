de0eda6305ddd4906e1816dd8285da28e3448062
BulyginK <buligin.k@gmail.com>
2022-03-25 17:45:57 +0300

    Внес первые правки

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 let num = 266219;
-let str = String(num);
-let arr = str.split('')
+let arr = String(num).split('')
 let multiply = 1;
-let degree;
-let strDegree;
 
 for (let i = 0; i < arr.length; i++) {
     multiply *= Number(arr[i])
 }
 
 console.log(multiply);
-
-degree = multiply ** 3
-strDegree = String(degree);
-
-console.log(strDegree.substring(0, 2)); 
+console.log(String(multiply ** 3).substring(0, 2)); 
