040b1eb3e13f4aef1623862e1e03b14e2a1ad198
BulyginK <bulygin.k@gmail.com>
2022-03-28 22:36:57 +0300

    Начал выполнение задания 3 урока

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-let num = 266219;
-let multiply = 1;
+'use strict';
 
-for (let i = 0; i < String(num).split('').length; i++) {
-    multiply *= Number(String(num).split('')[i]);
-}
-
-console.log(multiply);
-console.log(String(multiply ** 3).substring(0, 2)); 
