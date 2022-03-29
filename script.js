77fe2bde63a7367f3dcb34f5a910967d11a140f2
BulyginK <buligin.k@gmail.com>
2022-03-29 15:40:10 +0300

    внес правки

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 'use strict';
 
-let lang = ['ru', 'en'];
-const weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const week = [weekRu, weekEn]
-
-let langSelekt = prompt("Выбранный язык", "Если русский - ru, если английский - en");
-if (langSelekt === 'ru') {
-    lang = 'ru';
-} else if (langSelekt === 'en') {
-    lang = 'en';
-} else {
-    confirm("Ошибка в вводе данных");
-}
-
 // Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке
-
 // через if
+let lang = prompt("Введите выбранный язык", "ru или en");
 if (lang === 'ru') {
-    console.log(weekRu);
-} else if (lang === 'en') {
-    console.log(weekEn);
-} else {
-    console.log("Ошибка в вводе данных");
+    let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+    console.log(week);
+} 
+if (lang === 'en') {
+    let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    console.log(week);
 }
 
 //через switch-case
+lang = prompt("Введите выбранный язык", "ru или en");
 switch (lang) {
     case 'ru':
-        console.log(weekRu);
-        break
+        let weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+        console.log(weekRu)
+    break;
     case 'en':
-        console.log(weekEn);
-        break
-    default:
-        console.log("Ошибка в вводе данных");     
+        let weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        console.log(weekEn)
+    break;
 }
-
 // через многомерный массив без ифов и switch
-lang === 'ru' ? console.log(week[0]) : (lang === 'en') ? console.log(week[1]) : console.log("Ошибка в вводе данных");
+lang = prompt("Введите выбранный язык", "ru или en");
+let week = {
+    'ru':['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],  
+};
+console.log(week[lang])
 
 /*
 У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, 
@@ -46,4 +38,4 @@ lang === 'ru' ? console.log(week[0]) : (lang === 'en') ? console.log(week[1]) : 
 Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
 */
 let namePerson = prompt("Введите имя");
-namePerson === "Артем" ? console.log('директор') : (namePerson === "Александр") ? console.log('преподаватель') : console.log('студент');
+namePerson === "Артем" ? console.log('Директор') : (namePerson === "Александр") ? console.log('Преподаватель') : console.log('Студент');
