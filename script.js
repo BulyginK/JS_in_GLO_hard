044a941b56d25470b9b34aed6e996d929232057a
BulyginK <bulygin.k@gmail.com>
2022-04-03 15:49:49 +0300

    Выполнил 1 задание 5 урока

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // Для выполненения усложненных заданий
 
-const namber = prompt("Введите число");
+let namber;
+
+const isNamber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+function numberOutput() {
+    do {
+        namber = +prompt("Введите число");
+    } while (!isNamber(namber));
+    return namber;
+}
+
+isNamber();
+numberOutput();
+console.log(namber, typeof namber);
