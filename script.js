711c14212ec544f916052572516f0d0e1102ae3b
BulyginK <bulygin.k@gmail.com>
2022-04-03 21:30:14 +0300

    Выполнил 2 часть 2 задания 5 урока

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Для выполненения усложненных заданий
 
+// УСЛОЖНЕННОЕ ЗАДАНИЕ №2 1 задача
 const arr = ['199283', '283', '385742', '4762534', '5379', '64253', '4908967457']
 
 const newArr = arr.filter(function(item) {
@@ -7,3 +8,24 @@ const newArr = arr.filter(function(item) {
 })
 
 console.log(newArr);
+
+
+// УСЛОЖНЕННОЕ ЗАДАНИЕ №2 2 задача
+
+let n = 100;
+
+const sortOut = function() {
+    next:
+    for (let i = 2; i <= 100; i++) {
+        
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                continue next;
+            }
+        }
+        console.log(i + " Делители этого числа: 1 и " + i);
+    }
+    
+}
+
+sortOut();
