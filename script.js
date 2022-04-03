ffd96a412627da64845b59b31f43552532c482f0
BulyginK <bulygin.k@gmail.com>
2022-04-03 20:01:22 +0300

    Выполнил 1часть 2 задания 5 урока

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Для выполненения усложненных заданий
+
+const arr = ['199283', '283', '385742', '4762534', '5379', '64253', '4908967457']
+
+const newArr = arr.filter(function(item) {
+    return item.charAt(0) == 2 || item.charAt(0) == 4;
+})
+
+console.log(newArr);
