cbbb4df6dbbfa80947b98c1e04e91d781945d874
BulyginK <buligin.k@gmail.com>
2022-04-06 17:36:13 +0300

    Выполнил 7 допзадание

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Для выполненения усложненных заданий
 
 const weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const weekDiv = document.querySelector('week');
-const today = new Date();
+let today = new Date();
+let weekDay = getDay(today);
 
-console.log(today);
+const week = function() {
+    for (let i = 0; i < 7; i++) {
+        let div = document.createElement('p');
+        if(i < 5) {
+            div.innerHTML = `${weekArray[i]}`;
+        } else {
+            div.innerHTML = `${weekArray[i]}`;
+            div.style.fontStyle = 'italic';
+        }
+        document.body.append(div);
+    }
+}
+
+week();
+
+console.log(weekDay);
