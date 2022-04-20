8a6767e00acfe47ca25b0c88ffbbfedd15100ed6
BulyginK <buligin.k@gmail.com>
2022-04-20 18:21:51 +0300

    выполнил допзадание 7 урока

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 let today = new Date();
-let weekDay = today.getDay();
+let today_index = (6 + today.getDay()) % 7;
 
 
 const week = function() {
+    console.log(today_index);
+
     for (let i = 0; i < 7; i++) {
         let div = document.createElement('p');
         div.innerHTML = `${weekArray[i]}`;
         if (i > 4) {
             div.style.fontStyle = 'italic';
         }
-        if (i == weekDay) {
+        if (i == today_index ) {
             div.style.fontWeight = 'bold';
         }
         document.body.append(div);
